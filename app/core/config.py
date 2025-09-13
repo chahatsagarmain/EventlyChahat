9e418f334dc95ba80f078ecdcb38f3e63798533c
chahatsagarmain <chahatsagar2003@gmail.com>
2025-09-13 19:30:56 +0530

    docker composed it
    Signed-off-by: chahatsagarmain <chahatsagar2003@gmail.com>

--- app/core/config.py
+++ app/core/config.py
@@ -4,14 +4,14 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Evently (Async)"
     # asyncpg format: postgresql+asyncpg://user:password@host:port/dbname
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/evently"
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@db:5432/evently"
     JWT_SECRET: str = "secret_here"
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24  # 1 day
-    REDIS_HOST: str = "localhost"
+    REDIS_HOST: str = "cache"
     REDIS_PORT: int = 6379
-    MAILGUN_API_KEY: str = os.getenv("MAILGUN_API_KEY")
-    MAILGUN_DOMAIN: str = os.getenv("MAILGUN_DOMAIN")
+    MAILGUN_API_KEY: str = os.getenv("MAILGUN_API_KEY", "")
+    MAILGUN_DOMAIN: str = os.getenv("MAILGUN_DOMAIN", "")
     class Config:
         env_file = ".env"
 
